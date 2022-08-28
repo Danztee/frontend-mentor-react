fc416f784c58cba7910500779bffb2c3b368999d
Danztee <olowoniyidaniel@gmail.com>
2022-08-28 12:46:31 -0700

    added my own files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./components/styles/Container.styled";
+
+function App() {
+  return (
+    <Container className="App">
+      <h1>hello world</h1>
+    </Container>
+  );
+}
+
+export default App;
